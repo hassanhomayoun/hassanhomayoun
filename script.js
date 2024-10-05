e115f906bb11c77e97e05d43e21db0bf8fb425e5
hassanhomayoun <khastavaneh@hotmail.com>
2024-10-05 22:59:28 +0330

    Add files via upload
    Oct version, responsive menue

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
